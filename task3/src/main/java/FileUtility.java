b4e2f266315dcfe40661b479579936d0f7dcbf86
koblevmurat@gmail.com <koblevmurat@gmail.com>
2019-11-15 16:11:03 +0300

    реализация appender

--- task3/src/main/java/FileUtility.java
+++ task3/src/main/java/FileUtility.java
@@ -3,6 +3,27 @@ import java.util.*;
 
 public class FileUtility {
 
+
+
+    private void ReadFile(String _FileName) {
+
+      /*  try (BufferedWriter writer = new BufferedWriter(new FileWriter(_FileName))) {
+            for (int i = 0; i < 20; i++) {
+                writer.write("Java\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(_FileName))) {
+            String str;
+            while ((str = reader.readLine()) != null) {
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     /*
     * Структура файла ввода: в первой строке одно целое число - N
     * далее следует N целых чисел через пробел
@@ -37,7 +58,32 @@ public class FileUtility {
     * записи из списка по одной записи в строке
     * */
     public void appender(File file, List<String> records){
-        //TODO
+
+        try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
+
+            byte[] bb = new byte[(int)file.length()];
+            raf.readFully(bb);
+            raf.seek(0);
+            for (String record :
+                    records) {
+                raf.writeBytes(record);
+            }
+            raf.write(bb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return;
+
+      /*  try (BufferedWriter writer = new BufferedWriter(new FileWriter(file) )) {
+
+            for (String record :
+                    records) {
+                writer.append(record);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
     /*
